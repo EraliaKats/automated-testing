d0d048f953f60ee230fcd084a87a94460996f838
Eralia <eralia.katsakiori@outlook.com>
2022-02-23 11:21:34 +0000

    Fix bugs passes jest test

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -9,5 +9,5 @@ test("findLongestString finds the longest string in an array", () => {
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
   expect(findLongestString(["cat", "mat", "do"])).toBe("cat");
-  expect(findLongestString("eralia", "tree", "hurray")).toBe("eralia");
+  expect(findLongestString(["eralia", "tree", "hurray"])).toBe("eralia");
 });
